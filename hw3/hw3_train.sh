722e5ff509b5603dc21604f4b9ef79ce5082934e
jimlinntu <b04705003@ntu.edu.tw>
2017-11-17 09:36:49 +0800

    fix epochs number

--- hw3/hw3_train.sh
+++ hw3/hw3_train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 train.py JimResidualNetwork --train_filename $1 --n_epochs 1 --batch_size 32 
+python3 train.py JimResidualNetwork --train_filename $1 --n_epochs 100 --batch_size 32 
