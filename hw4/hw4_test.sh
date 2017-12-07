c27f3f6696464c495658ef9b893b37f3216a681d
jimlinntu <b04705003@ntu.edu.tw>
2017-12-08 00:11:29 +0800

    sh file

---
+++ hw4/hw4_test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 train.py no --test_filename $1 --testout $2 --load_model True
---
+++ hw4/hw4_train.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 train.py no --train_filename $1 --train_nolabel_filename $2 --param_folder ./param
